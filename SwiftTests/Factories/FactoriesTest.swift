d7f2e51cd567e3ab50d8325f1c2cb8140027cb4f
Wayne Bishop <wayne.bishop@arbutusinc.com>
2014-09-17 14:11:06 -0700

    Building test cases for the factory classes.

---
+++ SwiftTests/Factories/FactoriesTest.swift
@@ -0,0 +1,43 @@
+//
+//  FactoriesTest.swift
+//  SwiftStructures
+//
+//  Created by Wayne Bishop on 9/17/14.
+//  Copyright (c) 2014 Arbutus Software Inc. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class FactoriesTest: XCTestCase {
+
+    let numberList : Array<Int> = [8, 2, 10, 9, 7, 5]
+    
+    
+    func testLinkedList() {
+        
+        //create a new instance
+        var linkedList: LinkedList<Int> = LinkedList<Int>()
+
+        //append list items
+        for number in numberList {
+            linkedList.addLink(number)
+        }
+
+        
+        //print all the keys
+        linkedList.printAllKeys();
+        
+        
+        //note how the second parameter name is specified in the calling function
+        linkedList.addLinkAtIndex(4, index: 3)
+        
+        linkedList.printAllKeys()
+        
+        linkedList.removeLinkAtIndex(1)
+        linkedList.printAllKeys()
+
+        
+    }
+    
+}
