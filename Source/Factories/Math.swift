11f6b10bd9ca528cb36940582c07169b2593a23f
Wayne Bishop <wayne.bishop@arbutusinc.com>
2015-11-04 14:22:21 -0800

    Completed work on fibonacci algorithms..
    - includes examples of iterative, recursive and trailing closure techniques..

--- Source/Factories/Math.swift
+++ Source/Factories/Math.swift
@@ -10,10 +10,13 @@ import Foundation
 
 
 class Math {
+
+
+    //MARK: Fibonacci algorithms
     
     
-    //build fibonnaci sequence to a specified position
-    func fib(n: Int) ->Array<Int>! {
+    //build fibonacci sequence to a specified position - default
+    func fib(n: Int) -> Array<Int>! {
         
         
         if n < 2 {
@@ -41,26 +44,21 @@ class Math {
 
     
     
-    //build fibonnaci sequence to a specified position
-    func fibRecursive(n: Int, var sequence: Array<Int>!) {
+    //build fibonacci sequence to a specified position - recursive
+    func fib(n: Int, var sequence: Array<Int> = [0, 1]) {
 
         
         //initialize sequence
-        if sequence == nil || sequence.count < 2 {
-            sequence = [0, 1]
+        if n < 2 {
+            return
         }
-
+        
         
         let i: Int = sequence.count
         
         
         //set base condition
         if i == n {
-            
-            for s in sequence {
-                print("value is:\(s)")
-            }
-            
             return
         }
         
@@ -69,10 +67,40 @@ class Math {
 
         
         //set iteration
-        fibRecursive(n, sequence: sequence)
+        fib(n, sequence: sequence)
+        
+    
 
     }
     
+    
+    
+    //build fibonacci sequence to a specified position - trailing closure
+    func fib(n: Int, formula: Array<Int> -> Int) -> Array<Int>! {
+                
+        if n < 2 {
+            return nil
+        }
+        
+        
+        //initialize the sequence
+        var sequence: Array<Int> = [0, 1]
+        
+        var i: Int = sequence.count
+        
+        while i != n {
+            
+            let results: Int = formula(sequence)
+            sequence.append(results)
+            
+            i += 1
+        }
+
+        
+        return sequence
+        
+        
+    } //end function
     
     
 }
--- SwiftTests/MathTest.swift
+++ SwiftTests/MathTest.swift
@@ -18,32 +18,69 @@ class MathTest: XCTestCase {
 
     override func setUp() {
         super.setUp()
-        
     }
     
-    
-    //published example
-    func testFibonnaciEssayExample() {
+
+    //published example - default option
+    func testFibonnaci() {
         
         let positions: Int = 4
         let results: Array<Int>! = math.fib(positions)
         
-        
-        for s in results {
-            print("value is:\(s)")
-        }
-        
+        //test results
+        buildResultsTest(results)
         
     }
 
     
-    
+    //recursive option
     func testFibRecursive() {
         
-        let positions: Int = 4
+        let positions: Int = 9
         
         //set the number of iterations
-        math.fibRecursive(positions, sequence: nil)
+        math.fib(positions)
+        
+        
+    }
+    
+    
+    
+    //closure option
+    func testFibClosure() {
+        
+        let positions: Int = 23
+
+        
+        let results: Array<Int>! = math.fib(positions) { (sequence: Array<Int>!) -> Int in
+            
+            //initialize and set formula
+            let i: Int = sequence.count
+            let total: Int = sequence[i - 1] + sequence[i - 2]
+            
+            return total
+        }
+        
+
+        //test results
+        buildResultsTest(results)
+        
+    }
+    
+
+    
+    
+    //helper function - test results validity
+    func buildResultsTest(r: Array<Int>!) {
+
+        
+        if r == nil {
+            XCTFail("fibonnaci test failed..")
+        }
+        
+        if r[r.endIndex - 1] != r[r.endIndex - 2] + r[r.endIndex - 3] {
+            XCTFail("fibonnaci test failed..")
+        }
         
         
     }
