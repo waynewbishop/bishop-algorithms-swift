d71a3e58b4377fd110b262f1a4691e9abf9d7ee3
Matteo Piombo <matteo.piombo@fedro.org>
2015-12-20 11:01:06 +0100

    Hash Table simplification
    Remove implicitly unwrapped types
    Collapse some assignments in a single let foo = ...
    Minor changes to related tests

--- Source/Factories/HashTable.swift
+++ Source/Factories/HashTable.swift
@@ -11,102 +11,70 @@ import Foundation
 
 class HashTable {
     
-    private var buckets: Array<HashNode!>
+    private var buckets: Array<HashNode?>
     
     
     //initialize the buckets with nil values
     init(capacity: Int) {
-        self.buckets = Array<HashNode!>(count: capacity, repeatedValue:nil)
+        self.buckets = Array<HashNode?>(count: capacity, repeatedValue:nil)
         
     }
-
+    
     
     //add the key using the specified hash
     func addWord(firstname: String, lastname: String) {
         
-        var hashindex: Int!
-        var fullname: String!
-        
-        
         //create a hashvalue using the complete name
-        fullname = firstname + lastname
-        hashindex = self.createHash(fullname)
+        let fullname = firstname + lastname
+        let hashindex = self.createHash(fullname)
         
         
-        let childToUse: HashNode = HashNode()
-        var head: HashNode!
-        
-        
-        childToUse.firstname = firstname
-        childToUse.lastname = lastname
+        let childToUse: HashNode = HashNode(firstname: firstname, lastname: lastname)
         
         
         //check for an existing list
-        if  buckets[hashindex] == nil {
+        if let existingHead = buckets[hashindex] {
+            
+            //append existing chained list head to new item
+            childToUse.next = existingHead
+            
+            //update the chained list
+            buckets[hashindex] = childToUse
+            
+        } else {
+            // No collision, just add the node at hashindex
             buckets[hashindex] = childToUse
         }
         
-        else {
-            
-            print("a collision occured. implementing chaining..")
-            head = buckets[hashindex]
-            
-            
-            //append new item to the head of the list
-            childToUse.next = head
-            head = childToUse
-            
-            
-            //update the chained list
-            buckets[hashindex] = head
-        }
-
+        
         
     } //end function
     
     
     
     //determine if the word is found in the hash table
-    func findWord(firstname: String, lastname: String) -> Bool! {
+    func findWord(firstname: String, lastname: String) -> Bool {
         
-        var hashindex: Int!
-        var fullname: String!
-        
-        
-        fullname = firstname + lastname
-        hashindex = self.createHash(fullname)
+        let fullname = firstname + lastname
+        let hashindex = self.createHash(fullname)
         
         
         //determine if the value is present
-        if  buckets[hashindex] == nil {
-            print("name not found in hash table..")
-            return false
-        }
-        
+        if var node = buckets[hashindex] {
             
-        //iterate through the list of items to find a match
-        else {
-            
-            var current: HashNode! = buckets[hashindex]
-            
-            while (current != nil) {
-                
-                let hashName: String! = current.firstname + current.lastname
-                
-                if (hashName == fullname) {
-                    print("\(current.firstname) \(current.lastname) found in hash table..")
-                    return true
-                }
-                
+            while (node.firstname + node.lastname != fullname) {
                 
                 print("searching for word through chained list..")
-                current = current.next
-                
-                
-             } //end while
+                if let next = node.next {
+                    node = next
+                } else {
+                    return false
+                }
+            } //end while
             
-            
-        } //end if
+            print("\(node.firstname) \(node.lastname) found in hash table..")
+            return true
+        }
         
         
         print("name not found in hash table..")
@@ -116,13 +84,13 @@ class HashTable {
     
     
     
-   //return the hash value to be used
-   func createHash(fullname: String) -> Int! {
+    //return the hash value to be used
+    func createHash(fullname: String) -> Int {
         
         var remainder: Int = 0
         var divisor: Int = 0
         
-
+        
         for key in fullname.unicodeScalars {
             //println("the ascii value of \(key) is \(key.value)..")
             divisor += Int(key.value)
@@ -130,15 +98,13 @@ class HashTable {
         
         /*
         note: modular math is used to calculate a hash value. The bucket count is used
-        as the dividend to ensure all possible outcomes are between 0 and the collection size. 
+        as the dividend to ensure all possible outcomes are between 0 and the collection size.
         This is an example of a simple but effective hash algorithm.
         */
         
         remainder = divisor % buckets.count
         
-
+        
         return remainder - 1
     }
-    
-    
 }
